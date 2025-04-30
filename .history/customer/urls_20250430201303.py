f16d53fd9ff7f62f630505350bd4a450c1da6ac2
Ryan Walloh <ryanwalloh@outlook.com>
2025-04-30 22:17:53 +0800

    Refined order tracking

---
+++ .history/customer/urls_20250430201303.py
@@ -0,0 +1,14 @@
+# customer/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('vendors/', views.vendor_list, name='vendor_list'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('update_personal_details/', views.update_personal_details, name='update_personal_details'),
+    path('finalize_order/', views.finalize_order, name='finalize_order'),
+    path('order_complete/', views.order_complete, name='order_complete'),
+    path('save_address/', views.save_address, name='save_address'),
+    path('api/order-status/<int:order_id>/', views.get_order_status, name='get_order_status'),
+    
+]
