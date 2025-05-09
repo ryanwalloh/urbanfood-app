773f01998bd9103200240fba5cf1120f0a4b200a
Ryan Walloh <ryanwalloh@outlook.com>
2025-05-10 02:29:02 +0800

    Temporarily serve media files in production for testing

--- soti_delivery/urls.py
+++ soti_delivery/urls.py
@@ -24,6 +24,7 @@ from delivery.views import RiderLocationViewSet
 from core import views
 from django.conf import settings
 from django.conf.urls.static import static 
+from django.views.static import serve
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -49,6 +50,11 @@ urlpatterns = [
     
 
 ]
+
+if not settings.DEBUG:
+    urlpatterns += [
+        path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
