ef137d26bd7c01528183756bc87a028d07b5a33a
Ryan Walloh <ryanwalloh@outlook.com>
2025-05-09 17:43:11 +0800

    Add whitenoise and update static file settings

--- .history/soti_delivery/settings_20250509174059.py
+++ .history/soti_delivery/settings_20250509174059.py
@@ -98,7 +98,6 @@ WSGI_APPLICATION = 'soti_delivery.wsgi.application'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
