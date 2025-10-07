36f64537b46476cdecf362fc949defc763fbfdbd
Ryan Walloh <ryanwalloh@outlook.com>
2025-10-07 20:27:12 +0800

    Fix ASGI configuration: ensure Django settings are loaded before importing routing

--- soti_delivery/asgi.py
+++ soti_delivery/asgi.py
@@ -8,12 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soti_delivery.settings')
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from orders.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soti_delivery.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
