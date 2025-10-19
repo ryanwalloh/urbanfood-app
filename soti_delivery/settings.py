803c4ffbef5795426be0a089af611b70227e3817
Ryan Walloh <ryanwalloh@outlook.com>
2025-10-20 02:28:45 +0800

    SECURITY: Remove hardcoded Cloudinary API credentials from settings

--- soti_delivery/settings.py
+++ soti_delivery/settings.py
@@ -144,9 +144,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Cloudinary Configuration
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME', default='dwqrkobq1'),
-    'API_KEY': env('CLOUDINARY_API_KEY', default='947651824417687'),
-    'API_SECRET': env('CLOUDINARY_API_SECRET', default='xDRQUsaxLVOV2uNNMXVZGvOMfcQ'),
+    'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': env('CLOUDINARY_API_KEY'),
+    'API_SECRET': env('CLOUDINARY_API_SECRET'),
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
